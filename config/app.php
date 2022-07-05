c0d62b3269959538b5cf6998215c3d07e912d439
Jon Erickson <jon@deschutesdesigngroup.com>
2022-07-05 13:57:32 -0700

    Added laravel telescope

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\SparkServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 		App\Providers\TenancyServiceProvider::class
     ],
 
