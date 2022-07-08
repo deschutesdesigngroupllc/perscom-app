fe41d744eef991808a3a9721a1ac55661404c257
Jon Erickson <jon@deschutesdesigngroup.com>
2022-07-08 12:47:07 -0700

    Updated templates to conform with React

--- resources/js/components/Hero.jsx
+++ resources/js/components/Hero.jsx
@@ -27,8 +27,8 @@ export function Hero() {
             <div className="mt-10 flex justify-center space-x-6">
                 <ButtonLink href="/register">Get 1 week free</ButtonLink>
                 <ButtonLink href="mailto:sales@deschutesdesigngroup.com" variant="outline">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 flex-none" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 flex-none" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/>
                     </svg>
                     <span className="ml-3">Contact sales</span>
                 </ButtonLink>
