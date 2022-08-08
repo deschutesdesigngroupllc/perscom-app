b8be97a39905c80bf96f157ca089299cfd57553b
Jon Erickson <jon@deschutesdesigngroup.com>
2022-08-08 12:14:57 -0700

    Added tenant impersonation action

--- routes/tenant.php
+++ routes/tenant.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomainOrSubdomain;
+use Stancl\Tenancy\Features\UserImpersonation;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +18,15 @@ use Stancl\Tenancy\Middleware\InitializeTenancyByDomainOrSubdomain;
 */
 
 // Initialize tenancy
-Route::group(['middleware' => InitializeTenancyByDomainOrSubdomain::class], function () {
+Route::group(['middleware' => [InitializeTenancyByDomainOrSubdomain::class, 'web']], function () {
 	Route::get('/forms/{slug}', function ($slug) {
 		$form = \App\Models\Forms\Form::where('slug', $slug)->firstOrFail();
 		return redirect()->route('nova.pages.create', [
 			'resource' => \App\Nova\Forms\Submission::uriKey()
 		]);
 	})->name('form');
+
+	Route::get('/impersonate/{token}', function ($token) {
+		return UserImpersonation::makeResponse($token);
+	})->middleware();
 });
