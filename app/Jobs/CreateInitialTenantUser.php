e70e0da75afeeb3c0abf10e1e5d1742e2d8f1fec
Jon Erickson <jon@deschutesdesigngroup.com>
2022-11-17 16:06:16 -0800

    Rearranged files to fit better.

--- app/Jobs/CreateInitialTenantUser.php
+++ app/Jobs/CreateInitialTenantUser.php
@@ -2,7 +2,7 @@
 
 namespace App\Jobs;
 
-use App\Mail\NewTenantMail;
+use App\Mail\Tenant\NewTenantMail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
