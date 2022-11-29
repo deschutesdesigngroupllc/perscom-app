563116a221e25877a3af4cb235599e4bb6d2fa12
Jon Erickson <jon@deschutesdesigngroup.com>
2022-11-29 16:37:59 -0700

    Added ability to manually set users assignment attributes.

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
 use Laravel\Nova\Actions\ExportAsCsv;
 use Laravel\Nova\Fields\Badge;
+use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\Gravatar;
@@ -148,6 +149,14 @@ class User extends Resource
                     true => 'success',
                 ])
                 ->exceptOnForms(),
+	        Panel::make('Assignment', [
+		        BelongsTo::make('Position', 'position', Position::class)->help('You can manually set the user\'s position. Creating an assignment record will also change their position.')->nullable()->onlyOnForms(),
+		        BelongsTo::make('Specialty', 'specialty', Specialty::class)->help('You can manually set the user\'s specialty. Creating an assignment record will also change their specialty.')->nullable()->onlyOnForms(),
+		        BelongsTo::make('Unit', 'unit', Unit::class)->help('You can manually set the user\'.s unit. Creating an assignment record will also change their unit.')->nullable()->onlyOnForms()
+	        ]),
+	        Panel::make('Rank', [
+		        BelongsTo::make('Rank', 'rank', Rank::class)->help('You can manually set the user\'s rank. Creating a rank record will also change their rank.')->nullable()->onlyOnForms(),
+	        ]),
             Tabs::make('Personnel File', [
                 Tab::make('Demographics', [
                     Boolean::make('Email Verified', function () {
