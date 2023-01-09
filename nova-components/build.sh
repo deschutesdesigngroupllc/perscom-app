0daf7173d81a0988b05f4cd0286e7e7532e2cbe3
Jon Erickson <jon@deschutesdesigngroup.com>
2023-01-09 13:48:55 -0800

    Added build file to build all nova components for prod at once

---
+++ nova-components/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Builds assets for each nova component.
+# Executed by `npm run build-components`
+
+for d in ./nova-components/*/ ;
+    do (
+        # Enter each component folder.
+        cd "$d"
+
+        # Compille component assets.
+        if [ -f "package.json" ]; then
+            npm run prod
+        fi
+    );
+done
