b8a385e504f49bb42d3920ca5e1c09a8dd6966fb
Jon Erickson <jon@deschutesdesigngroup.com>
2023-01-19 11:34:02 -0700

    Reset email templates that got messed up with formatting

--- resources/views/emails/user/new-login.blade.php
+++ resources/views/emails/user/new-login.blade.php
@@ -1,16 +1,16 @@
 @component('mail::message')
-    # Your New Account Information
+# Your New Account Information
 
-    {{__('Your new account information has been successfully set up and your account details are included below. Please reach out if you have any questions or need help getting started.')}}
+{{__('Your new account information has been successfully set up and your account details are included below. Please reach out if you have any questions or need help getting started.')}}
 
-    **Dashboard URL**: [{{ $url }}]({{ $url }})<br>
-    **Email**: {{ $email }}<br>
-    **Password**: {{ $password }}
+**Dashboard URL**: [{{ $url }}]({{ $url }})<br>
+**Email**: {{ $email }}<br>
+**Password**: {{ $password }}
 
-    @component('mail::button', ['url' => $url])
-        Go To Dashboard
-    @endcomponent
+@component('mail::button', ['url' => $url])
+    Go To Dashboard
+@endcomponent
 
-    {{__('Thanks,')}}<br>
-    {{ config('app.name') }}
+{{__('Thanks,')}}<br>
+{{ config('app.name') }}
 @endcomponent
