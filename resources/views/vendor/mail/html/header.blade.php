bcfb8f8fb18512705e37abee4288bf96d589e74b
Jon Erickson <jon@deschutesdesigngroup.com>
2023-01-29 14:44:36 -0700

    Adjusted image in mail header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 <img srcset="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/logo-50.png') }}, {{ \Illuminate\Support\Facades\Vite::asset('resources/images/logo-100.png') }} 2x"
-     src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/logo-100.png') }}"
+     src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/logo-50.png') }}"
      alt="PERSCOM Personnel Management System">
 </a>
 </td>
