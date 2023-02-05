22d5930f2e3d90da5f449e8964c41568cbe91ed6
Jon Erickson <jon@deschutesdesigngroup.com>
2023-02-05 13:04:07 -0700

    Sort widget results based on order

--- app/Http/Controllers/Api/V1/Widget/AwardsController.php
+++ app/Http/Controllers/Api/V1/Widget/AwardsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1\Widget;
 
 use App\Http\Resources\Api\Widget\AwardResource;
 use App\Models\Award;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Orion\Http\Controllers\Controller;
 
 class AwardsController extends Controller
@@ -17,4 +19,20 @@ class AwardsController extends Controller
      * @var string
      */
     protected $resource = AwardResource::class;
+
+    /**
+     * Builds Eloquent query for fetching entities in index method.
+     *
+     * @param Request $request
+     * @param array $requestedRelations
+     * @return Builder
+     */
+    protected function buildIndexFetchQuery(Request $request, array $requestedRelations): Builder
+    {
+        $query = parent::buildIndexFetchQuery($request, $requestedRelations);
+
+        $query->ordered();
+
+        return $query;
+    }
 }
--- app/Http/Controllers/Api/V1/Widget/QualificationsController.php
+++ app/Http/Controllers/Api/V1/Widget/QualificationsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1\Widget;
 
 use App\Http\Resources\Api\Widget\QualificationResource;
 use App\Models\Qualification;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Orion\Http\Controllers\Controller;
 
 class QualificationsController extends Controller
@@ -17,4 +19,20 @@ class QualificationsController extends Controller
      * @var string
      */
     protected $resource = QualificationResource::class;
+
+    /**
+     * Builds Eloquent query for fetching entities in index method.
+     *
+     * @param Request $request
+     * @param array $requestedRelations
+     * @return Builder
+     */
+    protected function buildIndexFetchQuery(Request $request, array $requestedRelations): Builder
+    {
+        $query = parent::buildIndexFetchQuery($request, $requestedRelations);
+
+        $query->ordered();
+
+        return $query;
+    }
 }
