1a5efa02621d31c874abae7b14434d8c58d7d576
Jon Erickson <jon@deschutesdesigngroup.com>
2023-02-07 17:29:48 -0700

    Add heartbeat command for envoyer (#76)

---
+++ app/Console/Commands/RunEnvoyerHeartbeat.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class RunEnvoyerHeartbeat extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'perscom:heartbeat';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends a heartbeat to Envoyer.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if ($url = env('ENVOYER_HEARTBEAT_URL')) {
+            $response = Http::get($url);
+
+            if ($response->ok()) {
+                return Command::SUCCESS;
+            }
+
+            $this->error('There was an error when attempting the heartbeat.');
+
+            return Command::FAILURE;
+        }
+
+        $this->info('No heartbeat URL found.');
+
+        return Command::SUCCESS;
+    }
+}
