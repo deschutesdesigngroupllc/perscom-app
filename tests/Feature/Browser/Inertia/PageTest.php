2673672862b520ad863d6adc324ceb81e12d1e19
Jon Erickson <jon@deschutesdesigngroup.com>
2023-02-12 12:46:19 -0700

    Added test github action [skip ci]

--- tests/Feature/Browser/Inertia/PageTest.php
+++ tests/Feature/Browser/Inertia/PageTest.php
@@ -27,23 +27,6 @@ class PageTest extends TestCase
              })->assertSuccessful();
     }
 
-    public function test_register_completion_can_be_reached()
-    {
-        $this->post('/register', [
-            'organization' => $this->faker->company(),
-            'email' => $this->faker->companyEmail(),
-            'website' => $this->faker->url(),
-            'domain' => $this->faker->domainWord(),
-        ])->assertRedirectContains('/register/complete');
-    }
-
-    public function test_register_completion_redirect_back_without_tenant_data()
-    {
-        $this->get('/register/complete/'.random_int(1, 10))
-             ->assertRedirectToRoute('web.register.index')
-             ->assertRedirect('/register');
-    }
-
     public function test_page_not_found()
     {
         $this->get('/'.Str::random('5'))
