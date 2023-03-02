6c11cfc2e07d5bdd437289aca26221cf5dd15002
Jon Erickson <jon@deschutesdesigngroup.com>
2023-03-02 11:30:08 -0700

    Added command to set/get application version.

---
+++ app/Console/Commands/ApplicationVersion.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+
+class ApplicationVersion extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'perscom:version
+                            {--set= : Set the current application version}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Retrieves or sets the current application version.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $currentApplicationVersion = $this->getApplicationVersion();
+
+        $this->info("The current application version is: $currentApplicationVersion");
+
+        if ($newVersion = $this->option('set')) {
+            $this->setApplicationVersion($newVersion);
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param $version
+     * @return false|int
+     */
+    public function setApplicationVersion($version)
+    {
+        if (! preg_match("/^(v+)(\d|[1-9]\d*)\.(\d|[1-9]\d*)\.(\d|[1-9]\d*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/", $version)) {
+            return $this->error('The supplied version is not a valid SemVer version.');
+        }
+
+        $path = App::environmentFilePath();
+
+        $escaped = preg_quote('='.env('APP_VERSION'), '/');
+
+        $this->info("Setting the current application version to: $version");
+
+        return file_put_contents($path, preg_replace(
+            "/^APP_VERSION{$escaped}/m",
+            "APP_VERSION={$version}",
+            file_get_contents($path)
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getApplicationVersion()
+    {
+        return env('APP_VERSION');
+    }
+}
