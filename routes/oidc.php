7e6ac00ab280bf4d5aae84138f9df38169e15d61
Jon Erickson <jon@deschutesdesigngroup.com>
2023-04-10 14:32:06 -0600

    Add OIDC userinfo endpoint

--- routes/oidc.php
+++ routes/oidc.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Oidc\DiscoveryController;
+use App\Http\Controllers\Oidc\UserInfoController;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomainOrSubdomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
@@ -12,5 +13,8 @@ Route::group([
         'subscribed'
     ]], function () {
         Route::get('/.well-known/openid-configuration', [DiscoveryController::class, 'index']);
-    }
-);
+
+        Route::group(['middlware' => 'scope:openid'], function () {
+            Route::get('/userinfo', [UserInfoController::class, 'index']);
+        });
+});
---
+++ tests/Tenant/Feature/Http/Controllers/Oidc/UserInfoControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Tenant\Feature\Http\Controllers\Oidc;
+
+use App\Http\Middleware\Subscribed;
+use Laravel\Passport\Passport;
+use Tests\Tenant\TenantTestCase;
+
+class UserInfoControllerTest extends TenantTestCase
+{
+    public function test_userinfo_endpoint_can_be_reached()
+    {
+        $this->withoutMiddleware(Subscribed::class);
+
+        Passport::actingAs($this->user, [
+            'openid', 'profile', 'email',
+        ], 'passport');
+
+        $response = $this->get($this->tenant->url.'/oauth/userinfo')
+             ->assertSuccessful()
+             ->assertJson([
+                 'sub' => $this->user->getAuthIdentifier(),
+                 'email' => $this->user->email,
+                 'name' => $this->user->name,
+             ]);
+    }
+}
