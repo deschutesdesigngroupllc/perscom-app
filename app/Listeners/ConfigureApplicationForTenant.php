9c5491192fd6f9b3bb2d53a3723752de279cdb24
Jon Erickson <jon@deschutesdesigngroup.com>
2023-04-14 14:10:27 -0700

    Fixed settings table not existing test error

--- app/Listeners/ConfigureApplicationForTenant.php
+++ app/Listeners/ConfigureApplicationForTenant.php
@@ -4,7 +4,6 @@ namespace App\Listeners;
 
 use App\Models\Tenant;
 use Illuminate\Support\Facades\Config;
-use Outl1ne\NovaSettings\NovaSettings;
 use Spatie\Permission\PermissionRegistrar;
 
 class ConfigureApplicationForTenant
@@ -17,7 +16,7 @@ class ConfigureApplicationForTenant
         optional(\tenant(), static function (Tenant $tenant) {
             $tenant->run(function ($tenant) {
                 PermissionRegistrar::$cacheKey = 'spatie.permission.cache.tenant.'.$tenant->id;
-                Config::set('app.timezone', NovaSettings::getSetting('timezone', \config('app.timezone')));
+                Config::set('app.timezone', setting('timezone', \config('app.timezone')));
                 Config::set('mail.from.name', $tenant->name);
             });
         });
