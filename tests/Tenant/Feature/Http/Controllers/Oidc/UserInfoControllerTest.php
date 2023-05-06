595c37fa9d57b67a10d51175fc93ddf94e0e9a2a
Jon Erickson <jon@deschutesdesigngroup.com>
2023-05-06 12:35:16 -0600

    Unset API middleware from OIDC userinfo endpoint.

--- tests/Tenant/Feature/Http/Controllers/Oidc/UserInfoControllerTest.php
+++ tests/Tenant/Feature/Http/Controllers/Oidc/UserInfoControllerTest.php
@@ -2,15 +2,25 @@
 
 namespace Tests\Tenant\Feature\Http\Controllers\Oidc;
 
+use App\Http\Middleware\LogApiRequests;
+use App\Http\Middleware\SentryContext;
 use App\Http\Middleware\Subscribed;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 use Laravel\Passport\Passport;
 use Tests\Tenant\TenantTestCase;
+use Treblle\Middlewares\TreblleMiddleware;
 
 class UserInfoControllerTest extends TenantTestCase
 {
     public function test_userinfo_endpoint_can_be_reached()
     {
-        $this->withoutMiddleware(Subscribed::class);
+        $this->withoutMiddleware([
+            TreblleMiddleware::class,
+            SentryContext::class,
+            LogApiRequests::class,
+            ThrottleRequests::class,
+            Subscribed::class,
+        ]);
 
         Passport::actingAs($this->user, [
             'openid', 'profile', 'email',
