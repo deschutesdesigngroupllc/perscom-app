565ea045dc045c507314113d5d2099131d6493c7
Jon Erickson <jon@deschutesdesigngroup.com>
2023-05-18 11:43:17 -0600

    Format and build nova components

--- nova-components/Forms/nova.mix.js
+++ nova-components/Forms/nova.mix.js
@@ -13,19 +13,16 @@ class NovaExtension {
 
   webpackConfig(webpackConfig) {
     webpackConfig.externals = {
-      vue: 'Vue',
+      vue: 'Vue'
     }
 
     webpackConfig.resolve.alias = {
       ...(webpackConfig.resolve.alias || {}),
-      'laravel-nova': path.join(
-        __dirname,
-        '../../vendor/laravel/nova/resources/js/mixins/packages.js'
-      ),
+      'laravel-nova': path.join(__dirname, '../../vendor/laravel/nova/resources/js/mixins/packages.js')
     }
 
     webpackConfig.output = {
-      uniqueName: this.name,
+      uniqueName: this.name
     }
   }
 }
--- nova-components/Forms/resources/js/pages/Tool.vue
+++ nova-components/Forms/resources/js/pages/Tool.vue
@@ -1,20 +1,20 @@
 <template>
-    <div>
-        <Head title="Forms" />
-        <Heading class="mb-6">Forms</Heading>
-        <div id="perscom_widget_wrapper">
-            <component
-                is="script"
-                id="perscom_widget"
-                data-widget="forms"
-                :data-apikey="props.jwt"
-                :data-perscomid="props.tenant_id"
-                :src="props.widget_url"
-                type="text/javascript"
-            >
-            </component>
-        </div>
+  <div>
+    <Head title="Forms" />
+    <Heading class="mb-6">Forms</Heading>
+    <div id="perscom_widget_wrapper">
+      <component
+        is="script"
+        id="perscom_widget"
+        data-widget="forms"
+        :data-apikey="props.jwt"
+        :data-perscomid="props.tenant_id"
+        :src="props.widget_url"
+        type="text/javascript"
+      >
+      </component>
     </div>
+  </div>
 </template>
 
 <script setup>
