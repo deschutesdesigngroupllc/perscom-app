3fe4bd53fff3735ed40e0f1a27a7ce92d3c259ad
Jon Erickson <jon@deschutesdesigngroup.com>
2023-05-25 11:18:14 -0600

    Separated social login into distinct login and registration pathways (#174)
    * Separates social login to register and login functions so users cant register via the social providers on the login page - they have to explicitly use the social login via the register page. This way admins can disable registration and not have registrations happening via social providers.
    
    Fixes #173
    Fixes PERSCOM-IO-2F
    
    * Comments
    
    * Fix stan errors

--- app/Http/Controllers/SocialLoginController.php
+++ app/Http/Controllers/SocialLoginController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\LoginToken;
+use App\Models\Tenant;
 use App\Models\User;
 use App\Repositories\TenantRepository;
 use Carbon\Carbon;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialLoginController extends Controller
 {
+    /**
+     * Login function
+     */
+    public const SOCIAL_LOGIN = 'login';
+
+    /**
+     * Register function
+     */
+    public const SOCIAL_REGISTER = 'register';
+
     /**
      * @var string
      */
@@ -34,10 +47,11 @@ class SocialLoginController extends Controller
     /**
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function tenant($driver)
+    public function tenant($driver, $function)
     {
         return redirect()->route('auth.social.redirect', [
             'driver' => $driver,
+            'function' => $function,
             'tenant' => tenant()->getTenantKey(),
         ]);
     }
@@ -45,13 +59,12 @@ class SocialLoginController extends Controller
     /**
      * @return \Illuminate\Http\RedirectResponse|\Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function redirect($driver, $tenant)
+    public function redirect($driver, $function, Tenant $tenant)
     {
-        if (! $tenant) {
-            throw new \RuntimeException('There was no Tenant ID included with your request. Please try again.');
-        }
-
-        session()->put(self::$sessionKey, $tenant);
+        session()->put(self::$sessionKey, [
+            'tenant' => $tenant->getTenantKey(),
+            'function' => $function,
+        ]);
 
         return Socialite::driver($driver)->redirect();
     }
@@ -64,33 +77,58 @@ class SocialLoginController extends Controller
      */
     public function callback($driver, TenantRepository $tenantRepository)
     {
-        $tenantId = session()->get(self::$sessionKey);
+        $sessionData = session()->get(self::$sessionKey, []);
 
-        if (! $tenantId) {
-            throw new \RuntimeException('There was no Tenant ID saved to your session. Please try again.');
-        }
+        $tenantId = Arr::get($sessionData, 'tenant');
+        $function = Arr::get($sessionData, 'function');
 
         $socialLiteUser = Socialite::driver($driver)->user();
 
         $tenant = $tenantRepository->findById($tenantId);
-        $token = $tenant->run(function ($tenant) use ($socialLiteUser, $driver) {
-            $user = User::updateOrCreate([
-                'email' => $socialLiteUser->email,
-            ], [
-                'name' => $socialLiteUser->name,
-                'email_verified_at' => now(),
-                'social_id' => $socialLiteUser->id,
-                'social_driver' => $driver,
-                'social_token' => $socialLiteUser->token,
-                'social_refresh_token' => $socialLiteUser->refreshToken,
-            ]);
+        $token = $tenant->run(function ($tenant) use ($socialLiteUser, $driver, $function) {
+            switch ($function) {
+                case self::SOCIAL_LOGIN:
+                    $user = User::firstWhere('email', '=', $socialLiteUser->email);
+                    $user?->update([
+                        'social_id' => $socialLiteUser->id,
+                        'social_driver' => $driver,
+                        'social_token' => $socialLiteUser->token,
+                        'social_refresh_token' => $socialLiteUser->refreshToken,
+                    ]);
+                    break;
 
-            return LoginToken::create([
-                'user_id' => $user->id,
-            ]);
+                case self::SOCIAL_REGISTER:
+                    $user = User::create([
+                        'name' => $socialLiteUser->name,
+                        'email' => $socialLiteUser->email,
+                        'email_verified_at' => now(),
+                        'social_id' => $socialLiteUser->id,
+                        'social_driver' => $driver,
+                        'social_token' => $socialLiteUser->token,
+                        'social_refresh_token' => $socialLiteUser->refreshToken,
+                    ]);
+                    break;
+            }
+
+            if (isset($user)) {
+                $token = LoginToken::create([
+                    'user_id' => $user->id,
+                ]);
+            }
+
+            return $token ?? null;
         });
 
         session()->remove(self::$sessionKey);
+
+        if (! $token) {
+            $provider = Str::title($driver);
+            $query = Arr::query([
+                'status' => "We could not find an account associated with your $provider profile. Please create a new account to continue.",
+            ]);
+
+            return redirect()->to("{$tenant->url}/register?{$query}");
+        }
 
         return redirect()->to("{$tenant->url}/auth/login/$token->token");
     }
--- routes/auth.php
+++ routes/auth.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\SocialLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'feature:App\Features\SocialLoginFeature'], function () {
-    Route::get('/{driver}/{tenant}/redirect', [SocialLoginController::class, 'redirect'])
+    Route::get('/{driver}/{function}/{tenant}/redirect', [SocialLoginController::class, 'redirect'])
         ->name('social.redirect');
     Route::get('/{driver}/callback', [SocialLoginController::class, 'callback'])
         ->name('social.callback');
