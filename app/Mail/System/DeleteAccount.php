f9a35d1b66fbe38b62ea0b8ecf7b4018e8606134
Jon Erickson <jon@deschutesdesigngroup.com>
2023-05-30 16:21:33 -0600

    Inactive accounts now automatically removed (#176)
    * Add new remove inactive accounts job and command
    
    * Fixed stan errors

---
+++ app/Mail/System/DeleteAccount.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Mail\System;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteAccount extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.system.delete-account')->subject('Account Deleted');
+    }
+}
---
+++ app/Notifications/System/DeleteAccount.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Notifications\System;
+
+use App\Mail\System\DeleteAccount as DeleteAccountMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+class DeleteAccount extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * @return DeleteAccountMail
+     */
+    public function toMail(object $notifiable)
+    {
+        return (new DeleteAccountMail())->to($notifiable->email);
+    }
+}
