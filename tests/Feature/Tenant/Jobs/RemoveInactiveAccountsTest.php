a98998ea59e6f0e9bc97efa7c69f24fb41f8b1d5
Jon Erickson <jon@deschutesdesigngroup.com>
2023-07-26 20:23:26 -0600

    Refactor notification tests and add submission and task assignment observers tests

---
+++ tests/Feature/Tenant/Jobs/RemoveInactiveAccountsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Tenant\Jobs;
+
+use App\Jobs\RemoveInactiveAccounts;
+use App\Jobs\RemoveInactiveAccounts as RemoveInactiveAccountsJob;
+use App\Models\Tenant;
+use App\Notifications\System\DeleteAccountOneMonth;
+use Illuminate\Support\Facades\Notification;
+use Tests\Feature\Central\CentralTestCase;
+use Tests\Feature\Tenant\TenantTestCase;
+
+class RemoveInactiveAccountsTest extends CentralTestCase
+{
+    public function test_five_months_inactive_notification_sent()
+    {
+        Notification::fake();
+
+        $tenant = Tenant::factory()->state([
+            'last_login_at' => now()->subMonths(5)
+        ])->create();
+
+        RemoveInactiveAccountsJob::dispatchSync();
+
+        Notification::assertSentTo($tenant, DeleteAccountOneMonth::class, function ($notification, $channels) {
+            return true;
+        });
+    }
+}
