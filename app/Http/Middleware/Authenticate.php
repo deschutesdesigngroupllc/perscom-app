a42decb15b2d985b3dbd146d5e36839d2e8450dd
Jon Erickson <jon@deschutesdesigngroup.com>
2023-09-27 09:42:52 -0700

    WIP: new auth middleware to also check client credentials scope

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,38 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 class Authenticate extends Middleware
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+        // TODO: Finish this implementation
+        if (in_array('api', $guards)) {
+            try {
+                $clientCredentialMiddleware = app()->make(CheckClientCredentials::class);
+                return $clientCredentialMiddleware->handle($request, $next);
+            } catch (AuthenticationException $exception) {}
+        }
+
+        $this->authenticate($request, $guards);
+
+        return $next($request);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
