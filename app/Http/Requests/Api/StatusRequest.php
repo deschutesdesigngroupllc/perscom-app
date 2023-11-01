c92090a7895e6752fde8a7c324921f52716a3ab4
Jon Erickson <jon@deschutesdesigngroup.com>
2023-11-01 14:51:15 -0700

    Add status API endpoint

---
+++ app/Http/Requests/Api/StatusRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Orion\Http\Requests\Request;
+
+class StatusRequest extends Request
+{
+    /**
+     * @return string[]
+     */
+    public function commonRules(): array
+    {
+        return [
+            'name' => 'string',
+            'color' => 'string',
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function storeRules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'color' => 'required|string',
+        ];
+    }
+}
