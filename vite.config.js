9e473a908060073a5a730402da7883d7ba12e60b
Jon Erickson <jon@deschutesdesigngroup.com>
2023-11-01 15:14:08 -0700

    Add vite image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
+import {ViteImageOptimizer} from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
   plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
     laravel({
       input: ['resources/js/app.jsx', 'resources/css/nova.css'],
       refresh: true
-    })
+    }),
+    ViteImageOptimizer()
   ]
 })
