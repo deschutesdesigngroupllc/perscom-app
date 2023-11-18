8ae4d71d8c07eefa77635f5f37422b9b58994c5b
Jon Erickson <jon@deschutesdesigngroup.com>
2023-11-18 11:12:40 -0800

    Add foreign key constraints to all tenant tables

--- database/seeders/PassportSeeder.php
+++ database/seeders/PassportSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Laravel\Passport\ClientRepository;
 
 class PassportSeeder extends Seeder
 {
@@ -14,8 +13,8 @@ class PassportSeeder extends Seeder
      */
     public function run()
     {
-        $client = new ClientRepository();
-        $client->createPasswordGrantClient(null, 'Default Password Grant Client', 'http://your.redirect.path');
-        $client->createPersonalAccessClient(null, 'Default Personal Access Client', 'http://your.redirect.path');
+        //$client = new ClientRepository();
+        //$client->createPasswordGrantClient(null, 'Default Password Grant Client', 'http://your.redirect.path');
+        //$client->createPersonalAccessClient(null, 'Default Personal Access Client', 'http://your.redirect.path');
     }
 }
