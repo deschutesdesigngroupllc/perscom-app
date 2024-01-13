4ea6917e344609a931dfb6266ed949b2d64650ab
Jon Erickson <jon@deschutesdesigngroup.com>
2024-01-13 14:29:19 -0800

    Add null on delete to status FK constraint

---
+++ database/migrations/tenant/2024_01_13_221847_add_on_delete_null_status_id_in_assignment_records.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('records_assignments', function (Blueprint $table) {
+            $table->dropForeign('records_assignments_status_id_foreign');
+            $table->foreignId('status_id')->nullable()->change()->constrained('statuses')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('records_assignments', function (Blueprint $table) {
+            $table->dropForeign('records_assignments_status_id_foreign');
+            $table->foreignId('status_id')->nullable()->change()->constrained('statuses');
+        });
+    }
+};
