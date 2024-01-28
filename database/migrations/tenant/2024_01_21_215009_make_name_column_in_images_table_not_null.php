fdabec2c0a5d49a296c1f13466c6da6661e17643
Jon Erickson <jon@deschutesdesigngroup.com>
2024-01-28 13:22:33 -0800

    Fix failing migration (#301)

--- database/migrations/tenant/2024_01_21_215009_make_name_column_in_images_table_not_null.php
+++ database/migrations/tenant/2024_01_21_215009_make_name_column_in_images_table_not_null.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,6 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::table('images')->whereNull('name')->update(['name', '']);
+
         Schema::table('images', function (Blueprint $table) {
             $table->string('name')->nullable(false)->change();
         });
