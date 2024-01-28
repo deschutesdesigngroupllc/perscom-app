e0dace6502ae5aa22bb9bba51de5fa3d26f353b9
Jon Erickson <jon@deschutesdesigngroup.com>
2024-01-28 14:45:07 -0800

    wip

--- tests/Feature/Tenant/Models/TenantTest.php
+++ tests/Feature/Tenant/Models/TenantTest.php
@@ -2,80 +2,96 @@
 
 namespace Tests\Feature\Tenant\Models;
 
+use App\Models\Tenant;
 use Tests\Feature\Tenant\TenantTestCase;
 
 class TenantTest extends TenantTestCase
 {
     public function test_custom_domain_attribute_is_returned()
     {
-        $this->tenant->domains()->create([
+        $tenant = Tenant::factory()->createQuietly();
+        $tenant->domains()->createQuietly([
             'domain' => 'foo',
             'is_custom_subdomain' => true,
         ]);
-        $this->tenant->load('domains');
 
-        $this->assertNotNull($this->tenant->custom_domain);
-        $this->assertEquals('foo', $this->tenant->custom_domain->domain);
+        $this->assertNotNull($tenant->custom_domain);
+        $this->assertEquals('foo', $tenant->custom_domain->domain);
     }
 
     public function test_fallback_domain_attribute_is_returned()
     {
-        $this->assertNotNull($this->tenant->fallback_domain);
-        $this->assertEquals($this->tenant->domain->domain, $this->tenant->fallback_domain->domain);
+        $tenant = Tenant::factory()->createQuietly();
+        $tenant->domains()->createQuietly([
+            'domain' => $this->faker->word,
+        ]);
+
+        $this->assertNotNull($tenant->fallback_domain);
+        $this->assertEquals($tenant->domain->domain, $tenant->fallback_domain->domain);
     }
 
     public function test_domain_attribute_returns_custom_domain()
     {
-        $this->tenant->domains()->create([
+        $tenant = Tenant::factory()->createQuietly();
+        $tenant->domains()->createQuietly([
             'domain' => 'foo',
             'is_custom_subdomain' => true,
         ]);
-        $this->tenant->load('domains');
 
-        $this->assertEquals($this->tenant->custom_domain, $this->tenant->domain);
+        $this->assertEquals($tenant->custom_domain, $tenant->domain);
     }
 
     public function test_fallback_url_attribute_returns_proper_url()
     {
+        $tenant = Tenant::factory()->createQuietly();
+        $tenant->domains()->createQuietly([
+            'domain' => $this->faker->word,
+        ]);
+
         $scheme = config('app.scheme');
         $base = config('app.base_url');
 
-        $this->assertEquals("$scheme://{$this->tenant->fallback_domain->domain}$base", $this->tenant->fallback_url);
+        $this->assertEquals("$scheme://{$tenant->fallback_domain->domain}$base", $tenant->fallback_url);
     }
 
     public function test_custom_url_attribute_returns_proper_url()
     {
-        $this->tenant->domains()->create([
+        $tenant = Tenant::factory()->createQuietly();
+        $tenant->domains()->createQuietly([
             'domain' => 'foo',
             'is_custom_subdomain' => true,
         ]);
-        $this->tenant->load('domains');
 
         $scheme = config('app.scheme');
         $base = config('app.base_url');
 
-        $this->assertEquals("$scheme://{$this->tenant->custom_domain->domain}$base", $this->tenant->custom_url);
+        $this->assertEquals("$scheme://{$tenant->custom_domain->domain}$base", $tenant->custom_url);
     }
 
     public function test_url_attribute_properly_returns_domain()
     {
+        $tenant = Tenant::factory()->createQuietly();
+        $tenant->domains()->createQuietly([
+            'domain' => $this->faker->word,
+        ]);
+
         $scheme = config('app.scheme');
         $base = config('app.base_url');
 
-        $this->assertEquals("$scheme://{$this->tenant->domain->domain}$base", $this->tenant->url);
+        $this->assertEquals("$scheme://{$tenant->domain->domain}$base", $tenant->url);
     }
 
     public function test_url_attribute_properly_returns_custom_domain()
     {
-        $this->tenant->domains()->create([
+        $tenant = Tenant::factory()->createQuietly();
+        $tenant->domains()->createQuietly([
             'domain' => 'foo',
             'is_custom_subdomain' => true,
         ]);
-        $this->tenant->load('domains');
 
         $scheme = config('app.scheme');
         $base = config('app.base_url');
 
-        $this->assertEquals("$scheme://{$this->tenant->domain->domain}$base", $this->tenant->url);
+        $this->assertEquals("$scheme://{$tenant->domain->domain}$base", $tenant->url);
     }
 }
