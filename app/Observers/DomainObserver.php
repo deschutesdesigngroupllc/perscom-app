b480383a2f79ebb4dd00401618a53b116c4ce194
Jon Erickson <jon@deschutesdesigngroup.com>
2024-02-03 14:53:24 -0800

    wip

--- app/Observers/DomainObserver.php
+++ app/Observers/DomainObserver.php
@@ -6,12 +6,19 @@ use App\Models\Domain;
 use App\Notifications\System\DomainCreated;
 use App\Notifications\System\DomainDeleted;
 use App\Notifications\System\DomainUpdated;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
 
 class DomainObserver
 {
     public function created(Domain $domain): void
     {
+        Log::debug('Create', [
+            'tenant' => tenant(),
+            'domain' => $domain,
+            'backtrace' => debug_backtrace()
+        ]);
+
         Notification::send($domain->tenant, new DomainCreated($domain));
     }
 
@@ -22,6 +29,12 @@ class DomainObserver
 
     public function deleted(Domain $domain): void
     {
+        Log::debug('Deleted', [
+            'tenant' => tenant(),
+            'domain' => $domain,
+            'backtrace' => debug_backtrace()
+        ]);
+
         Notification::send($domain->tenant, new DomainDeleted($domain->url, $domain->tenant->url));
     }
 }
