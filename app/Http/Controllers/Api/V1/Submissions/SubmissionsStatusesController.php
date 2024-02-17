1605bbab84ad5b25cdb01da3f95ab50927549fd5
Jon Erickson <jon@deschutesdesigngroup.com>
2024-02-17 14:40:28 -0800

    Add a few more endpoints (#325)

---
+++ app/Http/Controllers/Api/V1/Submissions/SubmissionsStatusesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Submissions;
+
+use App\Http\Requests\Api\StatusRequest;
+use App\Models\Submission;
+use App\Policies\StatusPolicy;
+use Orion\Http\Controllers\RelationController;
+
+class SubmissionsStatusesController extends RelationController
+{
+    /**
+     * @var string
+     */
+    protected $model = Submission::class;
+
+    /**
+     * @var string
+     */
+    protected $request = StatusRequest::class;
+
+    /**
+     * @var string
+     */
+    protected $policy = StatusPolicy::class;
+
+    /**
+     * @var string
+     */
+    protected $relation = 'statuses';
+
+    /**
+     * @return string[]
+     */
+    public function alwaysIncludes(): array
+    {
+        return ['record'];
+    }
+}
---
+++ app/Http/Controllers/Api/V1/Users/UsersStatusRecordsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Users;
+
+use App\Http\Requests\Api\StatusRequest;
+use App\Models\User;
+use App\Policies\StatusPolicy;
+use Orion\Http\Controllers\RelationController;
+
+class UsersStatusRecordsController extends RelationController
+{
+    /**
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * @var string
+     */
+    protected $request = StatusRequest::class;
+
+    /**
+     * @var string
+     */
+    protected $policy = StatusPolicy::class;
+
+    /**
+     * @var string
+     */
+    protected $relation = 'statuses';
+
+    /**
+     * @return string[]
+     */
+    public function alwaysIncludes(): array
+    {
+        return ['record'];
+    }
+}
