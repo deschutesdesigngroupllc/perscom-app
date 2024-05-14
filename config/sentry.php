41afd2e077f7cbfc41159eec41e90235294875da
Jon Erickson <jon@deschutesdesigngroup.com>
2024-05-13 18:24:51 -0700

    Fix stan and tests

--- config/sentry.php
+++ config/sentry.php
@@ -57,4 +57,6 @@ return [
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#traces-sample-rate
     'traces_sample_rate' => env('SENTRY_TRACES_SAMPLE_RATE') === null ? null : (float) env('SENTRY_TRACES_SAMPLE_RATE'),
 
+    // @see: https://docs.sentry.io/platforms/php/guides/laravel/profiling/#enable-profiling
+    'profiles_sample_rate' => env('SENTRY_PROFILES_SAMPLE_RATE') === null ? null : (float) env('SENTRY_PROFILES_SAMPLE_RATE'),
 ];
