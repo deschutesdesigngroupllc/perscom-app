47da0fb7917eb58d716d99a35d55026e27b042d2
Jon Erickson <jon@deschutesdesigngroup.com>
2024-09-08 15:38:59 -0700

    Add action to email user

--- app/Filament/App/Resources/UserResource/Pages/ViewUser.php
+++ app/Filament/App/Resources/UserResource/Pages/ViewUser.php
@@ -6,6 +6,7 @@ namespace App\Filament\App\Resources\UserResource\Pages;
 
 use App\Filament\App\Resources\UserResource;
 use App\Filament\App\Resources\UserResource\RelationManagers;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -28,6 +29,11 @@ class ViewUser extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('email')
+                ->color('gray')
+                ->url(fn (?User $record) => "mailto:$record->email")
+                ->openUrlInNewTab()
+                ->tooltip(fn (?User $record) => $record->email),
             Actions\EditAction::make(),
         ];
     }
