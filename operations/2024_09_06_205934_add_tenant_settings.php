4a5a5fcaee74451ecdc68f114b5fef77ac7d26cc
Jon Erickson <jon@deschutesdesigngroup.com>
2024-09-14 08:47:26 -0700

    Add upgrade announcement

--- operations/2024_09_06_205934_add_tenant_settings.php
+++ operations/2024_09_06_205934_add_tenant_settings.php
@@ -11,6 +11,7 @@ use App\Settings\PermissionSettings;
 use App\Settings\RegistrationSettings;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
 
@@ -19,47 +20,51 @@ return new class extends OneTimeOperation
     public function process(): void
     {
         tenancy()->runForMultiple(Tenant::all(), function (Tenant $tenant) {
-            Artisan::call('tenants:migrate', [
-                '--tenants' => [$tenant->getTenantKey()],
-                '--path' => database_path('settings/tenant'),
-                '--force' => true,
-            ]);
+            try {
+                Artisan::call('tenants:migrate', [
+                    '--tenants' => [$tenant->getTenantKey()],
+                    '--path' => database_path('settings/tenant'),
+                    '--force' => true,
+                ]);
 
-            /** @var DashboardSettings $dashboardSettings */
-            $dashboardSettings = app(DashboardSettings::class);
-            $dashboardSettings->title = Settings::query()->where('name', 'dashboard_title')->pluck('value')->first() ?? $tenant->name;
-            $dashboardSettings->subtitle = Settings::query()->where('name', 'dashboard_subtitle')->pluck('value')->first() ?? null;
-            $dashboardSettings->subdomain = Settings::query()->where('name', 'subdomain')->pluck('value')->first() ?? null;
-            $dashboardSettings->domain = null;
-            $dashboardSettings->cover_photo_height = 100;
-            $dashboardSettings->save();
+                /** @var DashboardSettings $dashboardSettings */
+                $dashboardSettings = app(DashboardSettings::class);
+                $dashboardSettings->title = Settings::query()->where('name', 'dashboard_title')->pluck('value')->first() ?? $tenant->name;
+                $dashboardSettings->subtitle = Settings::query()->where('name', 'dashboard_subtitle')->pluck('value')->first() ?? null;
+                $dashboardSettings->subdomain = Settings::query()->where('name', 'subdomain')->pluck('value')->first() ?? null;
+                $dashboardSettings->domain = null;
+                $dashboardSettings->cover_photo_height = 100;
+                $dashboardSettings->save();
 
-            /** @var IntegrationSettings $integrationSettings */
-            $integrationSettings = app(IntegrationSettings::class);
-            $integrationSettings->single_sign_on_key = Settings::query()->where('name', 'single_sign_on_key')->pluck('value')->first() ?? Str::random(40);
-            $integrationSettings->save();
+                /** @var IntegrationSettings $integrationSettings */
+                $integrationSettings = app(IntegrationSettings::class);
+                $integrationSettings->single_sign_on_key = Settings::query()->where('name', 'single_sign_on_key')->pluck('value')->first() ?? Str::random(40);
+                $integrationSettings->save();
 
-            /** @var OrganizationSettings $organizationSettings */
-            $organizationSettings = app(OrganizationSettings::class);
-            $organizationSettings->name = Settings::query()->where('name', 'organization')->pluck('value')->first() ?? $tenant->name;
-            $organizationSettings->email = Settings::query()->where('name', 'email')->pluck('value')->first() ?? $tenant->email;
-            $organizationSettings->timezone = Settings::query()->where('name', 'timezone')->pluck('value')->first() ?? config('app.timezone');
-            $organizationSettings->save();
+                /** @var OrganizationSettings $organizationSettings */
+                $organizationSettings = app(OrganizationSettings::class);
+                $organizationSettings->name = Settings::query()->where('name', 'organization')->pluck('value')->first() ?? $tenant->name;
+                $organizationSettings->email = Settings::query()->where('name', 'email')->pluck('value')->first() ?? $tenant->email;
+                $organizationSettings->timezone = Settings::query()->where('name', 'timezone')->pluck('value')->first() ?? config('app.timezone');
+                $organizationSettings->save();
 
-            /** @var PermissionSettings $permissionSettings */
-            $permissionSettings = app(PermissionSettings::class);
-            $permissionSettings->default_permissions = json_decode(Settings::query()->where('name', 'default_permissions')->pluck('value')->first() ?? '[]', true) ?? [];
-            $permissionSettings->default_roles = json_decode(Settings::query()->where('name', 'default_roles')->pluck('value')->first() ?? '["User"]', true) ?? ['User'];
-            $permissionSettings->save();
+                /** @var PermissionSettings $permissionSettings */
+                $permissionSettings = app(PermissionSettings::class);
+                $permissionSettings->default_permissions = json_decode(Settings::query()->where('name', 'default_permissions')->pluck('value')->first() ?? '[]', true) ?? [];
+                $permissionSettings->default_roles = json_decode(Settings::query()->where('name', 'default_roles')->pluck('value')->first() ?? '["User"]', true) ?? ['User'];
+                $permissionSettings->save();
 
-            /** @var RegistrationSettings $registrationSettings */
-            $registrationSettings = app(RegistrationSettings::class);
-            $registrationSettings->enabled = (bool) Settings::query()->where('name', 'registration_enabled')->pluck('value')->first() ?? true;
-            $registrationSettings->admin_approval_required = (bool) Settings::query()->where('name', 'registration_admin_approval_required')->pluck('value')->first() ?? false;
-            $registrationSettings->save();
+                /** @var RegistrationSettings $registrationSettings */
+                $registrationSettings = app(RegistrationSettings::class);
+                $registrationSettings->enabled = (bool) Settings::query()->where('name', 'registration_enabled')->pluck('value')->first() ?? true;
+                $registrationSettings->admin_approval_required = (bool) Settings::query()->where('name', 'registration_admin_approval_required')->pluck('value')->first() ?? false;
+                $registrationSettings->save();
 
-            DB::statement('ALTER TABLE `settings` DROP COLUMN `value`');
-            DB::statement('DELETE FROM `settings` WHERE `group` IS NULL');
+                DB::statement('ALTER TABLE `settings` DROP COLUMN `value`');
+                DB::statement('DELETE FROM `settings` WHERE `group` IS NULL');
+            } catch (Exception $e) {
+                Log::debug($e);
+            }
         });
     }
 };
---
+++ operations/2024_09_14_154207_create_upgrade_announcement_from_v1_to_v2.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Announcement;
+use App\Models\Tenant;
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
+
+return new class extends OneTimeOperation
+{
+    public function process(): void
+    {
+        tenancy()->runForMultiple(Tenant::all(), function ($tenant) {
+            Announcement::create([
+                'global' => true,
+                'color' => 'info',
+                'title' => 'Welcome to PERSCOM, Version 2',
+                'content' => 'Please look around and check out the upgrade notes in our docs if you have questions.',
+                'expires_at' => now()->addWeeks(2),
+            ]);
+        });
+    }
+};
