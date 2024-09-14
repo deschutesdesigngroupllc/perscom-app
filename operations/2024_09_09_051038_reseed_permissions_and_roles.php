aeb0f4a1d32da282462a6dcb6d0697058ba5f9b0
Jon Erickson <jon@deschutesdesigngroup.com>
2024-09-14 13:14:45 -0700

    Switch reseeding to parallel

--- operations/2024_09_09_051038_reseed_permissions_and_roles.php
+++ operations/2024_09_09_051038_reseed_permissions_and_roles.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use BezhanSalleh\FilamentShield\Support\Utils;
 use Database\Seeders\ShieldSeeder;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Concurrency;
 use Illuminate\Support\Facades\DB;
 use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
 
@@ -14,7 +15,23 @@ return new class extends OneTimeOperation
 {
     public function process(): void
     {
-        tenancy()->runForMultiple(Tenant::all(), function (Tenant $tenant) {
+        $tenantIds = Tenant::pluck('id')->toArray();
+
+        $numChunks = 10;
+        $chunkSize = ceil(count($tenantIds) / $numChunks);
+        $tenantChunks = array_chunk($tenantIds, (int) $chunkSize);
+
+        $operations = [];
+        foreach ($tenantChunks as $tenantIds) {
+            $operations[] = fn () => Tenant::whereIn('id', $tenantIds)->get()->each(fn (Tenant $tenant) => $this->reseedTenant($tenant));
+        }
+
+        Concurrency::driver('fork')->run($operations);
+    }
+
+    protected function reseedTenant(Tenant $tenant): void
+    {
+        $tenant->run(function (Tenant $tenant) {
             $adminIds = User::role(Utils::getSuperAdminName())->get()->pluck('id');
 
             Schema::disableForeignKeyConstraints();
