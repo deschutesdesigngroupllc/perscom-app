d3a9f9545e5c42b284b9cad5671b5e0692d890e1
Jon Erickson <jon@deschutesdesigngroup.com>
2024-09-29 14:02:26 -0700

    Add version to dashboard (#492)

--- resources/js/landing/components/Footer.jsx
+++ resources/js/landing/components/Footer.jsx
@@ -10,23 +10,21 @@ export function Footer() {
         <div className='flex flex-col items-center py-16'>
           <Logo className='mx-auto h-10 w-auto' />
           <nav className='mt-10' aria-label='quick links'>
-            <ul className="-my-1 flex flex-wrap justify-center space-x-6">
+            <ul className='-my-1 flex flex-wrap justify-center space-x-6'>
               <li>
-                <a href={route('web.landing.home')} className="rounded-lg p-2.5 text-sm hover:bg-gray-200">
+                <a href={route('web.landing.home')} className='rounded-lg p-2.5 text-sm hover:bg-gray-200'>
                   {' '}
                   Home{' '}
                 </a>
               </li>
               <li>
-                <a href="https://docs.perscom.io" target="_blank" className="rounded-lg p-2.5 text-sm hover:bg-gray-200"
-                   rel="noreferrer">
+                <a href='https://docs.perscom.io' target='_blank' className='rounded-lg p-2.5 text-sm hover:bg-gray-200' rel='noreferrer'>
                   {' '}
                   Documentation{' '}
                 </a>
               </li>
               <li>
-                <a href={route('web.landing.home') + '#features'}
-                   className="rounded-lg p-2.5 text-sm hover:bg-gray-200">
+                <a href={route('web.landing.home') + '#features'} className='rounded-lg p-2.5 text-sm hover:bg-gray-200'>
                   {' '}
                   Features{' '}
                 </a>
@@ -34,46 +32,45 @@ export function Footer() {
               <li>
                 <a
                   href={route('web.find-my-organization.index')}
-                  target="_blank"
-                  className="rounded-lg p-2.5 text-sm hover:bg-gray-200"
-                  rel="noreferrer"
+                  target='_blank'
+                  className='rounded-lg p-2.5 text-sm hover:bg-gray-200'
+                  rel='noreferrer'
                 >
                   {' '}
                   Find My Organization{' '}
                 </a>
               </li>
               <li>
-                <a href={route('web.landing.home') + '#pricing'} className="rounded-lg p-2.5 text-sm hover:bg-gray-200">
+                <a href={route('web.landing.home') + '#pricing'} className='rounded-lg p-2.5 text-sm hover:bg-gray-200'>
                   {' '}
                   Pricing{' '}
                 </a>
               </li>
               <li>
-                <a href={route('web.privacy-policy.index')} className="rounded-lg p-2.5 text-sm hover:bg-gray-200">
+                <a href={route('web.privacy-policy.index')} className='rounded-lg p-2.5 text-sm hover:bg-gray-200'>
                   {' '}
                   Privacy Policy{' '}
                 </a>
               </li>
               <li>
-                <a href={route('web.slack')} className="rounded-lg p-2.5 text-sm hover:bg-gray-200">
+                <a href={route('web.slack')} className='rounded-lg p-2.5 text-sm hover:bg-gray-200'>
                   {' '}
                   Support{' '}
                 </a>
               </li>
               <li>
                 <a
-                  href="https://feedback.perscom.io/roadmap"
-                  target="_blank"
-                  className="rounded-lg p-2.5 text-sm hover:bg-gray-200"
-                  rel="noreferrer"
+                  href='https://feedback.perscom.io/roadmap'
+                  target='_blank'
+                  className='rounded-lg p-2.5 text-sm hover:bg-gray-200'
+                  rel='noreferrer'
                 >
                   {' '}
                   Roadmap{' '}
                 </a>
               </li>
               <li>
-                <a href="https://status.perscom.io" target="_blank"
-                   className="rounded-lg p-2.5 text-sm hover:bg-gray-200" rel="noreferrer">
+                <a href='https://status.perscom.io' target='_blank' className='rounded-lg p-2.5 text-sm hover:bg-gray-200' rel='noreferrer'>
                   {' '}
                   Status{' '}
                 </a>
@@ -81,8 +78,8 @@ export function Footer() {
             </ul>
           </nav>
         </div>
-        <div className="flex flex-col items-center border-t border-gray-400/10 py-10">
-          <p className="mt-6 text-center text-sm sm:mt-0">
+        <div className='flex flex-col items-center border-t border-gray-400/10 py-10'>
+          <p className='mt-6 text-center text-sm sm:mt-0'>
             Copyright &copy; {new Date().getFullYear()} Deschutes Design Group LLC. All rights reserved. reserved.
           </p>
         </div>
