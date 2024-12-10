95ebf97176ba786c4098766e9a1cab6142ed092e
Jon Erickson <jon@deschutesdesigngroup.com>
2024-12-09 19:08:36 -0800

    Fixes PERSCOM-APP-Y7

--- app/Filament/App/Resources/AssignmentRecordResource/Pages/CreateAssignmentRecord.php
+++ app/Filament/App/Resources/AssignmentRecordResource/Pages/CreateAssignmentRecord.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filament\App\Resources\AssignmentRecordResource\Pages;
 
 use App\Filament\App\Resources\AssignmentRecordResource;
+use App\Models\AssignmentRecord;
 use App\Models\ServiceRecord;
 use App\Traits\Filament\InteractsWithBatchRecords;
 use App\Traits\Filament\InteractsWithModelNotifications;
@@ -22,7 +23,7 @@ class CreateAssignmentRecord extends CreateRecord
     {
         $notificationData = data_get($data, 'model_notifications') ?? [];
 
-        $models = $this->performModelCreations(data_forget($data, 'model_notifications'), function (ServiceRecord $record) use ($notificationData) {
+        $models = $this->performModelCreations(data_forget($data, 'model_notifications'), function (AssignmentRecord $record) use ($notificationData) {
             $this->performModelNotificationInserts($record, $notificationData);
         });
 
