8ec1f214e0c92cf2972148f824444c12ba6192c2
Jon Erickson <jon@deschutesdesigngroup.com>
2025-02-02 15:55:23 -0800

    Refactor

--- app/Observers/StatusRecordObserver.php
+++ app/Observers/StatusRecordObserver.php
@@ -12,8 +12,9 @@ class StatusRecordObserver
     public function created(StatusRecord $statusRecord): void
     {
         if ($statusRecord->model instanceof User) {
-            $statusRecord->model->status_id = optional($statusRecord->status)->id;
-            $statusRecord->model->save();
+            $statusRecord->update([
+                'status_id' => $statusRecord->status_id,
+            ]);
         }
     }
 }
