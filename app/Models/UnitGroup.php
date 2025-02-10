610ada1dce3947bacec5a785c7d66d54f62b62e8
Jon Erickson <jon@deschutesdesigngroup.com>
2025-02-10 08:16:09 -0800

    Docs

--- app/Models/UnitGroup.php
+++ app/Models/UnitGroup.php
@@ -16,10 +16,14 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
  * @property int $group_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read Group $group
+ * @property-read Unit $unit
  *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup group(\App\Models\Group $group)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup unit(\App\Models\Unit $unit)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup whereGroupId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|UnitGroup whereId($value)
