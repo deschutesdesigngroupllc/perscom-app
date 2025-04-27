08b72118141cebc02c5bd68f48d53766ed46f484
Jon Erickson <jon@deschutesdesigngroup.com>
2025-04-26 18:17:54 -0700

    Add tests

--- app/Models/CompetencyCategory.php
+++ app/Models/CompetencyCategory.php
@@ -4,19 +4,37 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
 /**
- * @method static \Database\Factories\CompetencyCategoryFactory factory($count = null, $state = [])
+ * @property int $id
+ * @property int|null $competency_id
+ * @property int|null $category_id
+ * @property int $order
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory whereCompetencyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory whereOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CompetencyCategory whereUpdatedAt($value)
  *
  * @mixin \Eloquent
  */
-class CompetencyCategory extends Model
+class CompetencyCategory extends Pivot
 {
-    /** @use HasFactory<\Database\Factories\CompetencyCategoryFactory> */
-    use HasFactory;
+    protected $table = 'competencies_categories';
+
+    protected $fillable = [
+        'competency_id',
+        'category_id',
+        'order',
+        'created_at',
+        'updated_at',
+    ];
 }
--- database/factories/CompetencyFactory.php
+++ database/factories/CompetencyFactory.php
@@ -4,22 +4,23 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Models\Competency;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Competency>
+ * @extends Factory<Competency>
  */
 class CompetencyFactory extends Factory
 {
     /**
-     * Define the model's default state.
-     *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
+        $competency = "Competency {$this->faker->unique()->randomNumber()}";
+
         return [
-            //
+            'name' => $competency,
         ];
     }
 }
