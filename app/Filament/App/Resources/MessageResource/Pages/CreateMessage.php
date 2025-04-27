ece1f752726a51f9b220d9d2d3a33ca2f76dc9d1
Jon Erickson <jon@deschutesdesigngroup.com>
2025-04-26 21:04:36 -0700

    Fix message creation wizard

--- app/Filament/App/Resources/MessageResource/Pages/CreateMessage.php
+++ app/Filament/App/Resources/MessageResource/Pages/CreateMessage.php
@@ -5,9 +5,30 @@ declare(strict_types=1);
 namespace App\Filament\App\Resources\MessageResource\Pages;
 
 use App\Filament\App\Resources\MessageResource;
+use Filament\Forms;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMessage extends CreateRecord
 {
+    use CreateRecord\Concerns\HasWizard;
+
     protected static string $resource = MessageResource::class;
+
+    public function getSteps(): array
+    {
+        return [
+            Forms\Components\Wizard\Step::make('Message')
+                ->completedIcon('heroicon-m-hand-thumb-up')
+                ->schema(MessageResource::messageSchema()),
+            Forms\Components\Wizard\Step::make('Channels')
+                ->completedIcon('heroicon-m-hand-thumb-up')
+                ->schema(MessageResource::channelSchema()),
+            Forms\Components\Wizard\Step::make('Details')
+                ->completedIcon('heroicon-m-hand-thumb-up')
+                ->schema(MessageResource::detailsSchema()),
+            Forms\Components\Wizard\Step::make('Schedule')
+                ->completedIcon('heroicon-m-hand-thumb-up')
+                ->schema(MessageResource::scheduleSchema()),
+        ];
+    }
 }
