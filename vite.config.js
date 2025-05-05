2d22328ffcaa5faddaed4e6540d427a31201b4ca
Jon Erickson <jon@deschutesdesigngroup.com>
2025-05-05 15:39:32 -0700

    Fix build errors

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return
+        }
+        warn(warning)
+      }
+    }
+  },
   server: {
     cors: true
   },
