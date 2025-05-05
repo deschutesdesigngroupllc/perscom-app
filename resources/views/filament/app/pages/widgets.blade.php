dcedd66e71cef02fa86b4bdf63631ffac79ca0f1
Jon Erickson <jon@deschutesdesigngroup.com>
2025-05-05 15:22:30 -0700

    Formatting

--- resources/views/filament/app/pages/widgets.blade.php
+++ resources/views/filament/app/pages/widgets.blade.php
@@ -1,19 +1,19 @@
 <x-filament-panels::page>
   <div x-data="{
-      init() {
-          const script = document.createElement('script');
-          script.id = 'perscom_widget';
-          script.src = '{{ config('app.widget_url') }}';
-          script.type = 'text/javascript';
-          script.setAttribute('data-apikey', '{{ $this->apiKey }}');
-          script.setAttribute('data-widget', '{{ $this->widget }}');
-  
-          if (document.documentElement.classList.contains('dark')) {
-              script.setAttribute('data-dark', 'true');
-          }
-  
-          document.getElementById('perscom_widget_wrapper')?.appendChild(script);
+    init() {
+      const script = document.createElement('script');
+      script.id = 'perscom_widget';
+      script.src = '{{ config('app.widget_url') }}';
+      script.type = 'text/javascript';
+      script.setAttribute('data-apikey', '{{ $this->apiKey }}');
+      script.setAttribute('data-widget', '{{ $this->widget }}');
+
+      if (document.documentElement.classList.contains('dark')) {
+        script.setAttribute('data-dark', 'true');
       }
+
+      document.getElementById('perscom_widget_wrapper')?.appendChild(script);
+    }
   }" x-init="init">
     <div id="perscom_widget_wrapper"></div>
   </div>
