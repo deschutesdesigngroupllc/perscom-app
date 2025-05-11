ed2291aed419850cf50964e32dfd88c0345606d4
Jon Erickson <jon@deschutesdesigngroup.com>
2025-05-11 16:26:31 -0700

    Add dynamic generation of field options from resource (#608)
    * Add dynamic generation of field options from resource
    
    * Organize nav links
    
    * Add timezones

--- app/Models/Field.php
+++ app/Models/Field.php
@@ -13,10 +13,11 @@ use App\Traits\ClearsApiCache;
 use App\Traits\ClearsResponseCache;
 use App\Traits\HasResourceLabel;
 use App\Traits\HasResourceUrl;
-use ArrayObject;
+use ArrayObject as ArrayObjectAlias;
 use Filament\Support\Contracts\HasLabel;
-use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Casts\ArrayObject;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
@@ -43,12 +44,12 @@ use function in_array;
  * @property FieldOptionsModel|null $options_model
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection<int, Form> $forms
+ * @property-read Collection<int, Form> $forms
  * @property-read int|null $forms_count
  * @property-read string $label
  * @property-read string|null $relative_url
  * @property-read string|null $url
- * @property-read \Illuminate\Database\Eloquent\Collection<int, User> $users
+ * @property-read Collection<int, User> $users
  * @property-read int|null $users_count
  * @property-read string|null $validation_rules
  *
@@ -109,6 +110,30 @@ class Field extends Model implements HasLabel, Hideable
     ];
 
     /**
+     * @return Attribute<ArrayObject, never>
+     */
+    public function options(): Attribute
+    {
+        return Attribute::get(function ($value, $attributes = null): ArrayObject {
+            if (filled($value) && data_get($attributes, 'options_type') === FieldOptionsType::Array->value) {
+                return new ArrayObject(json_decode($value), ArrayObjectAlias::ARRAY_AS_PROPS);
+            }
+
+            if (filled(data_get($attributes, 'options_model')) && $this->options_model instanceof FieldOptionsModel && data_get($attributes, 'options_type') === FieldOptionsType::Model->value) {
+                return new ArrayObject($this->options_model->getOptions(), ArrayObjectAlias::ARRAY_AS_PROPS);
+            }
+
+            if (data_get($attributes, 'type') === FieldType::FIELD_TIMEZONE->value) {
+                return new ArrayObject(Collection::wrap(timezone_identifiers_list())
+                    ->mapWithKeys(fn ($timezone) => [$timezone => $timezone])
+                    ->toArray(), ArrayObjectAlias::ARRAY_AS_PROPS);
+            }
+
+            return new ArrayObject([], ArrayObjectAlias::ARRAY_AS_PROPS);
+        })->shouldCache();
+    }
+
+    /**
      * @return Attribute<?string, never>
      */
     public function validationRules(): Attribute
@@ -159,7 +184,6 @@ class Field extends Model implements HasLabel, Hideable
             'readonly' => 'boolean',
             'required' => 'boolean',
             'type' => FieldType::class,
-            'options' => AsArrayObject::class,
             'options_type' => FieldOptionsType::class,
             'options_model' => FieldOptionsModel::class,
         ];
