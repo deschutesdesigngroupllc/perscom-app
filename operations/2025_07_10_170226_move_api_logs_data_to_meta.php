67d434626932c91f1383aeaaeade7801b895bc6e
Jon Erickson <jon@deschutesdesigngroup.com>
2025-07-10 11:30:35 -0700

    Optimize migration script

--- operations/2025_07_10_170226_move_api_logs_data_to_meta.php
+++ operations/2025_07_10_170226_move_api_logs_data_to_meta.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use App\Models\ApiLog;
 use App\Models\Tenant;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
 
 return new class extends OneTimeOperation
@@ -12,16 +14,40 @@ return new class extends OneTimeOperation
     public function process(): void
     {
         tenancy()->runForMultiple(Tenant::all(), function (): void {
-            ApiLog::query()->lazy()->each(function (ApiLog $log): void {
-                $log->setMeta(Arr::only($log->properties->toArray(), [
-                    'ip',
-                    'method',
-                    'duration',
-                    'endpoint',
-                    'status',
-                    'request_id',
-                    'trace_id',
-                ]));
+            ApiLog::query()->select(['id', 'properties'])->chunkById(1000, function (Collection $logs): void {
+                $metaInserts = [];
+
+                /** @var ApiLog $log */
+                foreach ($logs as $log) {
+                    $meta = Arr::only($log->properties->toArray(), [
+                        'ip',
+                        'method',
+                        'duration',
+                        'endpoint',
+                        'status',
+                        'request_id',
+                        'trace_id',
+                    ]);
+
+                    foreach ($meta as $key => $value) {
+                        $metaInserts[] = [
+                            'key' => $key,
+                            'value' => $value,
+                            'type' => gettype($value),
+                            'status' => 1,
+                            'owner_type' => ApiLog::class,
+                            'owner_id' => $log->id,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                        ];
+                    }
+                }
+
+                if ($metaInserts !== []) {
+                    DB::transaction(function () use ($metaInserts): void {
+                        DB::table('meta')->insert($metaInserts);
+                    });
+                }
             });
         });
     }
