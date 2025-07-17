fe8a7cecf797acc08a8061bbc469acc1cd3cc5e9
Jon Erickson <jon@deschutesdesigngroup.com>
2025-07-17 12:18:23 -0700

    Remove old API meta logs

---
+++ operations/2025_07_17_191353_remove_old_api_meta_logs.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\ApiLog;
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
+
+return new class extends OneTimeOperation
+{
+    public function process(): void
+    {
+        Model::unsetEventDispatcher();
+
+        tenancy()->runForMultiple(Tenant::all(), function (): void {
+            $min = ApiLog::query()->min('id');
+
+            DB::query()
+                ->from('meta')
+                ->where('owner_type', ApiLog::class)
+                ->where('owner_id', '<', $min)
+                ->delete();
+        });
+    }
+};
