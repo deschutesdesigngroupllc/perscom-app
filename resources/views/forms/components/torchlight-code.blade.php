9db4ef1b18878df3240b194abb3325999f7a2ff6
Jon Erickson <jon@deschutesdesigngroup.com>
2025-12-09 12:53:28 -0800

    Wip

--- resources/views/forms/components/torchlight-code.blade.php
+++
@@ -1,9 +0,0 @@
-<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
-  <pre>
-    <x-torchlight-code
-        language="{{ $field->getLanguage() ?? '' }}"
-        theme="{{ $field->getTheme() ?? '' }}"
-        {{ $attributes }}
-    >{!! $getState() !!}</x-torchlight-code>
-</pre>
-</x-dynamic-component>
--- resources/views/forms/components/widget-code-generator.blade.php
+++
@@ -1,8 +0,0 @@
-<div x-ignore ax-load ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('widget-code-generator') }}"
-  x-on:update-code.window="updateCode($event.detail.code)" x-data="widgetCodeGenerator({
-      state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }}
-  })">
-  <x-filament::button :color="$getColor()" x-on:click="copyCode().then(() => $tooltip('Copied'))">
-    {{ $getLabel() }}
-  </x-filament::button>
-</div>
