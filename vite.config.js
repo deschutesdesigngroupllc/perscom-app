3424adbeb2bef622ca81ccda9ff42a29be630e94
Jon Erickson <jon@deschutesdesigngroup.com>
2025-12-13 15:27:30 -0800

    Add source maps

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
+  build: {
+    sourcemap: true
+  },
   plugins: [
     tailwindcss(),
     laravel({
