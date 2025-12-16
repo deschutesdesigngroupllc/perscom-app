caebecc7a42ab1e320da280313aed483960e2cf3
Jon Erickson <jon@deschutesdesigngroup.com>
2025-12-15 21:09:26 -0800

    Add settings

---
+++ app/Settings/FieldSettings.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class FieldSettings extends Settings
+{
+    public ?array $assignment_records = null;
+
+    public ?array $award_records = null;
+
+    public ?array $combat_records = null;
+
+    public ?array $qualification_records = null;
+
+    public ?array $rank_records = null;
+
+    public ?array $service_records = null;
+
+    public ?array $training_records = null;
+
+    public static function group(): string
+    {
+        return 'fields';
+    }
+}
