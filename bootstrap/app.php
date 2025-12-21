8e35b3398c3d9e75a490dd5c5b1f3b2fdfb9a5ad
jonerickson <3356740+jonerickson@users.noreply.github.com>
2025-12-21 06:40:59 +0000

    chore: apply code style fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -76,7 +76,7 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->redirectGuestsTo(fn () => route('filament.app.auth.login'));
+        $middleware->redirectGuestsTo(fn (): string => route('filament.app.auth.login'));
 
         $middleware->validateCsrfTokens(except: [
             'spark/*',
