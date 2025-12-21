f4fce077af6f5f7635c241a6f8889eb7405a7e2f
Jon Erickson <jon@deschutesdesigngroup.com>
2025-12-21 15:05:45 -0800

    Reset permissions

---
+++ operations/2025_12_21_224850_reset_permissions.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\Tenant;
+use Database\Seeders\ShieldSeeder;
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
+
+return new class extends OneTimeOperation
+{
+    public function process(): void
+    {
+        tenancy()->runForMultiple(Tenant::all(), function (Tenant $tenant) {
+            Schema::disableForeignKeyConstraints();
+
+            Role::truncate();
+            Permission::truncate();
+
+            Artisan::call('tenants:seed', [
+                '--tenants' => $tenant->getTenantKey(),
+                '--class' => ShieldSeeder::class,
+            ]);
+
+            Schema::enableForeignKeyConstraints();
+        });
+    }
+};
