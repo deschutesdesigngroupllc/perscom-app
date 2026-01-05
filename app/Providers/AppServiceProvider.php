bd0118df0f8fcc72d223b990572eeb1b4e8cfd63
Jon Erickson <jon@deschutesdesigngroup.com>
2026-01-04 21:43:16 -0800

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,6 +43,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Number;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -352,5 +353,7 @@ class AppServiceProvider extends ServiceProvider
         Table::configureUsing(function (Table $table): void {
             $table->defaultSort('created_at', 'desc');
         });
+
+        URL::forceHttps(App::isProduction());
     }
 }
