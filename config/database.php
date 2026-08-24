623ab03cad8243967872c72f90ea2e3d472f1551
Jon Erickson <jon@deschutesdesigngroup.com>
2026-08-23 21:29:02 -0600

    Add username to other redis connections

--- config/database.php
+++ config/database.php
@@ -180,6 +180,7 @@ return [
         'session' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_SESSION_DB', '2'),
@@ -189,6 +190,7 @@ return [
         'queue' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_QUEUE_DB', '3'),
